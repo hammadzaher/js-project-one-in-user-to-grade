34271298c7328af601a2ee25fd600e3722a5dbc1
hammadzaher <zaheerhammad62@gmail.com>
2024-09-13 10:22:17 +0500

    js project1

---
+++ hammad/script.js
@@ -0,0 +1,10 @@
+function submitForm() {
+	let input1 = document.getElementById('input1').value;
+	let input2 = document.getElementById('input2').value;
+	let input3 = document.getElementById('input3').value;
+	let input4 = document.getElementById('input4').value;
+	let input5 = document.getElementById('input5').value;
+	let input6 = document.getElementById('input6').value;
+	
+	alert("Input 1: " + input1 + "\nInput 2: " + input2 + "\nInput 3: " + input3 + "\nInput 4: " + input4 + "\nInput 5: " + input5 + "\nInput 6: " + input6);
+}
